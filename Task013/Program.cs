5fc19af013a909b3a90c1a8597b24b8ededaf3ff
Krasowskaya <krasovskayameow@gmail.com>
2022-10-08 18:45:32 +0300

    add new tasks

---
+++ Task013/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if(a < 100)
+    Console.WriteLine("Третья цифра отсутствует");
+else 
+    Console.WriteLine(Convert.ToString(a)[2]);
+    
---
+++ Task015/Program.cs
@@ -0,0 +1,14 @@
+﻿
+
+Console.Clear();
+Console.WriteLine("Введите цифру, соответствующую дню недели: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if(a < 1 || a > 7)
+    Console.WriteLine("Попробуй еще раз!");
+
+else if( a >= 1 && a <= 5)
+    Console.WriteLine("Нет!");
+
+else if( a >= 6 || a <= 7)
+   Console.WriteLine("Да!");
