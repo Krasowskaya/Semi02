0649d638bb6036a7c82cc6567b77fbd6ed96158f
Krasowskaya <krasovskayameow@gmail.com>
2022-10-12 21:12:56 +0300

    Заменила задачи на семинар 3

---
+++ Task019/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 19 Напишите пjограмму, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+Console.Clear();
+Console.WriteLine("Введите пятизначное число: ");
+string array = Convert.ToString(Console.ReadLine());
+
+    if(array[0]==array[4] && array[1]==array[3])
+        Console.WriteLine("Pal");
+    else
+        Console.WriteLine("no");
