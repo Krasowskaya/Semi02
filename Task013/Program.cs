1a73a80c24a5ce48a1e1a99f72de39f92d7eb601
Krasowskaya <krasovskayameow@gmail.com>
2022-10-12 21:15:37 +0300

    обновила

--- Task013/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿Console.Clear();
-Console.WriteLine("Введите число: ");
-int a = Convert.ToInt32(Console.ReadLine());
-
-if(a < 100)
-    Console.WriteLine("Третья цифра отсутствует");
-else 
-    Console.WriteLine(Convert.ToString(a)[2]);
-    
